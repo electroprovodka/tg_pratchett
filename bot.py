8a432fb286540c2ec7a30bee246e4c21b5a8a648
electroprovodka <9608266+electroprovodka@users.noreply.github.com>
2024-03-11 22:24:39 +0100

    Update bot to use more quotes

--- bot.py
+++ bot.py
@@ -1,10 +1,18 @@
 from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from telegram.ext import filters, MessageHandler, ApplicationBuilder, ContextTypes, CommandHandler
 import csv
 import os
 import random
 from datetime import datetime, timezone
+import logging
 
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+logger = logging.getLogger(__name__)
 
 DISABLE_THROTTLING = os.getenv("DISABLE_THROTTLING", False)
 TELEGRAM_API_KEY = os.environ["TELEGRAM_API_KEY"]
@@ -14,8 +22,22 @@ QUOTES_FILE = "./quotes.csv"
 DB_FILE = "./database.csv"
 
 START_QUOTE = "Welcome, traveler! You've just turned a page into a rather unexpected chapter. Let's see where the words take us, shall we?"
-MISSING_QUOTE = "It seems we've rummaged through every drawer and peeked under every magical hat, only to find that our quote vault stands empty. Fear not, for the Discworld is vast and full of tales yet untold. Give us a bit of time to refill the inkwell and bind some new pages. Check back later for more slices of wisdom and whimsy."
-THROTTLING_QUOTE = "Remember, the turtle moves slowly but surely. Return on the morrow for another morsel of wisdom. Until then, good night!"
+MISSING_QUOTES = [
+    "It seems we've rummaged through every drawer and peeked under every magical hat, only to find that our quote vault stands empty. Fear not, for the Discworld is vast and full of tales yet untold. Give us a bit of time to refill the inkwell and bind some new pages. Check back later for more slices of wisdom and whimsy.",
+]
+THROTTLING_QUOTE = [
+    "Remember, the turtle moves slowly but surely. Return on the morrow for another morsel of wisdom. Until then, good night!",
+        "The sands of our hourglass have run low for today. Swing by on the morrow for another gem!",
+    "Every tale needs a pause. Return with the dawn for your next chapter of wisdom.",
+    "The stars say it's time to rest. A fresh quote awaits with the morning light.",
+    "As the Disc rests, so shall our quest for quotes. Tomorrow, the adventure continues!",
+]
+
+RANDOM_RESPONSES = [
+    "Ah, it seems my Luggage has wandered off with that understanding. Could you charm me with a /quote command instead, just until it returns?",
+    "Your spell has tickled my curiosity, but my magical interpreter is out sipping tea. Perhaps a /quote command might clear the mist?",
+    "I've wandered into a curious dimension where your words puzzle me. Could we navigate back with a /quote command, just to ensure we're on the same page?",
+]
 
 MISSING_TYPE = "missing"
 THROTTLED_TYPE = "throttled"
@@ -84,36 +106,57 @@ def select_quote(user_id: str) -> str:
 def get_quote(user_id: str) -> str:
     quote_id = select_quote(user_id)
     if quote_id == MISSING_TYPE:
-        print(f"Missing quote {user_id}")
-        return MISSING_QUOTE
+        logger.warning(f"Missing quote {user_id}")
+        return random.choice(MISSING_QUOTES)
     elif quote_id == THROTTLED_TYPE:
-        print(f"Throttled {user_id}")
-        return THROTTLING_QUOTE
+        logger.info(f"Throttled {user_id}")
+        return random.choice(THROTTLING_QUOTE)
     
     mark_as_seen(user_id, quote_id)
     return QUOTES[quote_id]
 
 
+
+async def send(context: ContextTypes.DEFAULT_TYPE, chat_id: str, text: str) -> None:
+    # Retry 3 times
+    for _ in range(3):
+        try:
+            await context.bot.send_message(chat_id=chat_id, text=text)
+            break
+        except Exception as e:
+            logger.error(f"Error {e}")
+            continue
+
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    print(f"Start {update.effective_chat.id}")
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=START_QUOTE)
+    logger.info(f"Start {update.effective_chat.id}")
+    await send(context, chat_id=update.effective_chat.id, text=START_QUOTE)
 
 async def new_quote(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    print(f"Quote {update.effective_chat.id}")
+    logger.info(f"Quote {update.effective_chat.id}")
     
     user_id = update.effective_chat.id
     quote_text = get_quote(str(user_id))
 
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=quote_text)
+    await send(context, chat_id=update.effective_chat.id, text=quote_text)
+
+
+async def random_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    reply = random.choice(RANDOM_RESPONSES)
+    await send(context, chat_id=update.effective_chat.id, text=reply)
 
 
 if __name__ == "__main__":
+    logger.info("Starting...")
     application = ApplicationBuilder().token(TELEGRAM_API_KEY).build()
 
     start_handler = CommandHandler('start', start)
     quote_handler = CommandHandler('quote', new_quote)
+    random_message_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), random_message)
+
     application.add_handler(start_handler)
     application.add_handler(quote_handler)
+    application.add_handler(random_message_handler)
 
     application.run_polling()
 
